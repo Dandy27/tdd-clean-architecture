daf5ec1f73553536032a846269e2cc8e6ab8574f
dandy <danielsbarbosa27@gmail.com>
2021-10-01 16:59:38 -0300

    correcao

--- tdd_clean_architecture/lib/features/domain/usecase/get_validaton_drink.dart
+++ tdd_clean_architecture/lib/features/domain/usecase/get_validaton_drink.dart
@@ -1,7 +1,7 @@
 import 'package:tdd_clean_architecture/features/domain/entities/weight_drink.dart';
 import 'package:tdd_clean_architecture/features/domain/repositories/validation.repository.dart';
 
-final WeightDrink weightDrink;
+
 
 
 class GetValidatonDrink {
@@ -10,8 +10,5 @@ class GetValidatonDrink {
   GetValidatonDrink(this.validationRepository);
 
 
-  if () {
-    
-  } else {
-  }
+  
 }
